1ea94ce315fa5c5e0db6f037561749f6a0e7f694
wgliang <liangcszzu@163.com>
2016-04-16 23:23:50 -0400

    add data

--- testdata.py
+++ testdata.py
@@ -3,11 +3,11 @@ import redis,codecs
 r = redis.Redis(host='114.215.85.245',port=6379,db=0)
 
 count = 6243
-fod = open("./experiment/alphago.txt", "w")
+fod = open("./experiment/alphago1.txt", "w")
 while 1:
     print "================",count
     data = r.lindex('comments-alphago',count)
-    fod.write("'"+data+"',0\n")
+    fod.write("'"+data+"',1\n")
     count = count - 1
     if count == -1:
         break
