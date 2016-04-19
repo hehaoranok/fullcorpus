d9a3a23a6bb7d0d4749597fed88a03691c2d2bfe
wgliang <liangcszzu@163.com>
2016-04-19 02:24:37 -0400

    add

--- sentence/mlf.py
+++ sentence/mlf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import jieba,codecs
-fow = codecs.open("../word/word.txt", "r", encoding='UTF-8')
+fow = codecs.open("../word/seg.txt", "r", encoding='UTF-8')
 fos = codecs.open("coae2014.txt", "r", encoding='UTF-8')
 count = 4999
 index = 1
@@ -24,10 +24,14 @@ print "++++++++++++++++++++++++++++++++++++++++++"
 while 1:
     line = fos.readline()
     vector = ""
+    flag = 0
     if line:
         line = line.strip()
         tive =  line[-1]
-        vector = vector + tive
+        if int(tive) == 1:
+            tive = "+1"
+        else:
+            tive =  "-1"
         line3 = line[1:-3]
         seg_list = jieba.cut(line3, cut_all=True)
         for key in seg_list:
@@ -35,13 +39,19 @@ while 1:
             key = key.strip()
             if dict.has_key(key):
                 #print key
+                flag = 1
                 dict[key] = 1
                 #vector = vector + " " + str(dict[key]) + ":1"
         for i in xrange(len(list)):
             if dict[list[i]] == 1:
-                vector = vector + " " + str(i) + ":1"
+                vector = vector + " " +"1"
                 dict[list[i]] = 0
-        print vector
+            else:
+                vector = vector + " " + "0"
+        vector = vector + " " + tive
+        if flag == 1:
+            print vector
+            fod.write(vector+"\n")
         count = count + 1
     else:
         break
