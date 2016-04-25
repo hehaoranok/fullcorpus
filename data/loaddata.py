aee5220a48502771ec285079abf4c17c43d8777f
wgliang <liangcszzu@163.com>
2016-04-25 01:36:58 -0400

    load all data

--- data/loaddata.py
+++ data/loaddata.py
@@ -8,14 +8,15 @@ sys.setdefaultencoding("utf-8")
 print sys.getdefaultencoding() 
 r = redis.Redis(host='114.215.85.245',port=6379,db=0)
 
-fod = codecs.open("alphago.txt", "a+", encoding='UTF-8')
-count = 6243
+#fod = codecs.open("pingce-data.txt", "a+", encoding='UTF-8')
+fod = open("pingce-answer.txt", "a+")
+count = 4999
 
 while 1:
-    val = r.lindex('comments-alphago',count)
+    val = r.lindex('pingce-answer',count)
     if val:
-        print val
-        fod.write(str(val)+"\n")
+        #print val
+        fod.write(val+"\n")
     count = count - 1
     if count == -1:
         break
